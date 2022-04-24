4652493ebfd151ac2ecb656e8379b7eefd790c2f
TaneriX07 <tanerivince@gmail.com>
2022-04-24 22:18:45 +0800

    First commit

---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import './Header.css'
+
+const Header = () => {
+  return (
+    <header>
+      <div class="logo">
+        <span>Quick Quizes</span>
+      </div>
+      <div class="highscores">
+        <span>View Highscores</span>
+      </div>
+    </header>
+  )
+}
+
+export default Header
